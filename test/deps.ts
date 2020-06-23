3d0e8e45e7850670dd51b900c6cfab57cfd522c4
asos-craigmorten <craig.morten@asos.com>
2020-06-23 18:57:38 +0100

    chore: update test dependencies.

--- test/deps.ts
+++ test/deps.ts
@@ -1,5 +1,5 @@
 // Deliberately test bleeding edge to ensure compatibility with latest / upcoming
-export * from "https://deno.land/x/oak@master/mod.ts";
-export { expect } from "https://deno.land/x/expect@9effa6c6da3bcf4b66114b44e6b1662e85c91337/mod.ts";
-export { dirname, join } from "https://deno.land/std@0.53.0/path/mod.ts";
+export * from "https://deno.land/x/oak@v5.3.1/mod.ts";
+export { expect } from "https://deno.land/x/expect@285caf/mod.ts";
+export { dirname, join } from "https://deno.land/std@0.58.0/path/mod.ts";
 export { isFreePort } from "https://deno.land/x/free_port@v1.2.0/mod.ts";
