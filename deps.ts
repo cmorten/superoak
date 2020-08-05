ddbec25e31d1087ecb53beb8cc5009bc9a086884
Craig Morten <46491566+asos-craigmorten@users.noreply.github.com>
2020-08-05 09:05:43 +0100

    [NO-ISSUE] Deno registry fixes (#7)

--- deps.ts
+++ deps.ts
@@ -1,10 +1,10 @@
-export { Server } from "https://deno.land/std@0.61.0/http/server.ts";
+export { Server } from "https://deno.land/std@0.63.0/http/server.ts";
 export {
   superdeno,
   SuperDeno,
   Test,
   IRequest,
   IResponse,
-} from "https://deno.land/x/superdeno@2.0.0/mod.ts";
-export { methods } from "https://deno.land/x/opine@0.19.1/mod.ts";
+} from "https://deno.land/x/superdeno@2.1.1/mod.ts";
+export { methods } from "https://deno.land/x/opine@0.20.2/mod.ts";
 export { getFreePort } from "https://deno.land/x/free_port@v1.2.0/mod.ts";
