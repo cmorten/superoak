1ebcef28d33a54346713700358653d2aaeb55853
Craig Morten <cmorten@users.noreply.github.com>
2020-12-13 14:57:59 +0000

    [NO-ISSUE] Support redirects API (#16)

--- deps.ts
+++ deps.ts
@@ -1,9 +1,9 @@
 export { Server } from "https://deno.land/std@0.80.0/http/server.ts";
-export { superdeno, Test } from "https://deno.land/x/superdeno@2.5.0/mod.ts";
+export { superdeno, Test } from "https://deno.land/x/superdeno@3.0.0/mod.ts";
 export type {
   IRequest,
   IResponse,
   SuperDeno,
-} from "https://deno.land/x/superdeno@2.5.0/mod.ts";
+} from "https://deno.land/x/superdeno@3.0.0/mod.ts";
 export { methods } from "https://deno.land/x/opine@0.27.0/mod.ts";
 export { getFreePort } from "https://deno.land/x/free_port@v1.2.0/mod.ts";
