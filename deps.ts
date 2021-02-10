fea336d93cc2e33619581fab52f7f0ccad7efeac
cmorten <craig.morten@hotmail.co.uk>
2021-02-10 08:52:35 +0000

    fix: wait for listen promise to resolve

--- deps.ts
+++ deps.ts
@@ -1,9 +1,9 @@
 export { Server } from "https://deno.land/std@0.85.0/http/server.ts";
-export { superdeno, Test } from "https://deno.land/x/superdeno@3.1.0/mod.ts";
+export { superdeno, Test } from "https://deno.land/x/superdeno@3.1.1/mod.ts";
 export type {
   IRequest,
   IResponse,
   SuperDeno,
-} from "https://deno.land/x/superdeno@3.1.0/mod.ts";
+} from "https://deno.land/x/superdeno@3.1.1/mod.ts";
 export { methods } from "https://deno.land/x/opine@1.1.0/mod.ts";
 export { getFreePort } from "https://deno.land/x/free_port@v1.2.0/mod.ts";
