4c9682844f877bb52ed100c145489d779f4e9263
cmorten <craig.morten@hotmail.co.uk>
2021-07-13 17:37:30 +0100

    feat: 4.4.0

--- test/deps.ts
+++ test/deps.ts
@@ -1,4 +1,4 @@
-export { dirname, join } from "https://deno.land/std@0.99.0/path/mod.ts";
-export * from "https://deno.land/x/oak@v7.6.3/mod.ts";
+export { dirname, join } from "https://deno.land/std@0.101.0/path/mod.ts";
+export * from "https://deno.land/x/oak@v7.7.0/mod.ts";
 export { expect } from "https://deno.land/x/expect@v0.2.9/mod.ts";
 export { isFreePort } from "https://deno.land/x/free_port@v1.2.0/mod.ts";
